60ad17eb6704d19ccb3e3405c9f85e4f390d0812
kubk <ya.7gorbachev2016@yandex.ru>
2017-03-12 23:47:52 +0000

    Using responsive Bootstrap navbar, update publish-frontend.sh

--- scripts/publish-frontend.sh
+++ scripts/publish-frontend.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-mkdir public/css
-ln -sf `pwd`/vendor/twbs/bootstrap/dist/css/bootstrap.min.css public/css/bootstrap.min.css
-ln -sf `pwd`/vendor/twbs/bootstrap/dist/fonts public/
+mkdir public/vendor public/vendor/{css,js}
+
+ln -sf `pwd`/vendor/twbs/bootstrap/dist/css/bootstrap.min.css public/vendor/css/bootstrap.min.css
+ln -sf `pwd`/vendor/twbs/bootstrap/dist/js/bootstrap.min.js public/vendor/js/bootstrap.min.js
+ln -sf `pwd`/vendor/components/jquery/jquery.min.js public/vendor/js/jquery.min.js
+ln -sf `pwd`/vendor/twbs/bootstrap/dist/fonts public/vendor/
