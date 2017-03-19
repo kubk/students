ff96ee835be7075af354ac340a9f1c245b1a8dec
kubk <ya.7gorbachev2016@yandex.ru>
2017-03-19 08:08:46 +0000

    Added COMMENT statement

--- create-students-table.sql
+++ create-students-table.sql
@@ -2,16 +2,19 @@ DROP TABLE IF EXISTS students;
 DROP TYPE IF EXISTS genderEnum;
 
 CREATE TYPE genderEnum AS ENUM ('f', 'm');
+
 CREATE TABLE students (
   id      SERIAL PRIMARY KEY,
   name    VARCHAR(25)  NOT NULL,
   surname VARCHAR(30)  NOT NULL,
   email   VARCHAR(60)  NOT NULL,
-  gender genderEnum NOT NULL,
+  gender  genderEnum   NOT NULL,
   "group" VARCHAR(5)   NOT NULL,
   rating  INT          NOT NULL,
-  token   VARCHAR(255) NOT NULL, -- Registration token
+  token   VARCHAR(255) NOT NULL,
   CONSTRAINT email UNIQUE (email),
   CONSTRAINT token UNIQUE (token),
-  CONSTRAINT rating_constraint CHECK (rating <= 200 AND rating > 0)
+  CONSTRAINT rating_constraint CHECK (rating <= 200 AND rating >= 0)
 );
+
+COMMENT ON COLUMN students.token IS 'Registration token';
